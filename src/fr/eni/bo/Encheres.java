bc16a3056d3bc514676c94d8f1762db6f3eb4aef
Yannis Haismann <72089971+YannisHaismann@users.noreply.github.com>
2021-07-21 14:29:51 +0200

    Catégorie Dal Bo Bll & test

---
+++ src/fr/eni/bo/Encheres.java
@@ -0,0 +1,36 @@
+package fr.eni.bo;
+
+import java.sql.Date;
+
+public class Encheres {
+	private int idUtilisateur;
+	private int idArticle;
+	private Date date;
+	private int montant;
+	
+	public int getIdUtilisateur() {
+		return idUtilisateur;
+	}
+	public void setIdUtilisateur(int idUtilisateur) {
+		this.idUtilisateur = idUtilisateur;
+	}
+	public int getIdArticle() {
+		return idArticle;
+	}
+	public void setIdArticle(int idArticle) {
+		this.idArticle = idArticle;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public int getMontant() {
+		return montant;
+	}
+	public void setMontant(int montant) {
+		this.montant = montant;
+	}
+	
+}
